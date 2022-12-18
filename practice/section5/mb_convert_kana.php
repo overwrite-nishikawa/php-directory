f69f9bedf5846d39039da65d1ac856c3f915301a
Kisara Nishikawa <nishikawa@overwrite.work>
2022-12-18 19:09:32 +0900

    組み込み関数　trim~array_searchまで追加

---
+++ practice/section5/mb_convert_kana.php
@@ -0,0 +1,8 @@
+<?php
+// $str = 'WINGSﾌﾟﾛｼﾞｪｸﾄ';
+// print mb_convert_kana($str, 'RKV');
+
+// 以下、自分で書いたコード
+$str = 'WINGSプロジェクト';
+
+print mb_convert_kana($str,'RKV');
---
+++ practice/section5/trim.php
@@ -0,0 +1,33 @@
+<?php
+// $str = "  こんにちは \t\n\r";
+// var_dump($str);
+// print '<br />';
+// var_dump(trim($str));
+// print '<br />';
+// var_dump(ltrim($str));
+// print '<br />';
+// var_dump(rtrim($str));
+// print '<br />';
+
+// $str2 = '!======［独習PHP］======!';
+// var_dump(trim($str2, "!=［］"));
+
+// 以下、自分で書いたコード
+// $str = "  こんにちは \t\n\r";
+// var_dump($str);
+// print '<br />';
+// var_dump(trim($str));
+// print '<br />';
+// var_dump(ltrim($str));
+// print '<br />';
+// var_dump(rtrim($str));
+// print '<br />';
+
+// $str2 = '!===== [独習PHP] =====!';
+// var_dump(trim($str2,"!= []"));
+
+// printf('%sは%sです','ニンザブロウ','ハムスター');
+// printf('売上平均(前月比):%+0-8.3f',0.196785);
+// printf('売上平均(前月比):%'*10.3e",0.198765);
+
+
