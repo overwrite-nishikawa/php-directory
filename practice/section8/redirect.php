30b8af86fa77b046f8cc6cc2819114655f578d24
Kisara Nishikawa <nishikawa@overwrite.work>
2023-01-05 23:12:27 +0900

    req_headers.php~redirect.phpまで追加

---
+++ practice/section8/redirect.php
@@ -0,0 +1,5 @@
+<?php
+// header('Location: http://localhost/selfphp/chap08/req_headers.php');
+// header('Location: http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).'/req_headers.php');
+header('Location: http://localhost/selfphp/chap08/req_headers.php');
+header('Location: http://' .$_SERVER['HTTP_HPST.dirname($_SERVER['PHP_SELF']).'])
