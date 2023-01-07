732bdf8f2318b528eb057672c0fbb66363b04538
Kisara Nishikawa <nishikawa@overwrite.work>
2023-01-08 01:03:02 +0900

    chase.phpとauthorize.phpを追加

---
+++ practice/section8/authorize.php
@@ -0,0 +1,29 @@
+<?php
+// if (!isset($_SERVER['PHP_AUTH_USER'])) {
+//   header('HTTP/1.1 401 Unauthorized');
+//   header('WWW-Authenticate: Basic realm="SelfPHP"');
+//   print 'この画面へのアクセスは認められませんでした。';
+//   die();
+// } else {
+//   if ($_SERVER['PHP_AUTH_USER'] === 'admin_usr' &&
+//     $_SERVER['PHP_AUTH_PW'] === 'admin_pass') {
+//     print '正しく認証が行われました。';
+//   } else {
+//     print 'ユーザー名、またはパスワードが間違っています。';
+//   }
+// }
+
+// 自分で書いたコード
+if(!isset($_SERVER['PHP_AUTH_USER'])) {
+  header('HTTP/1.1 401 unauthorized');
+  header('WWW-Authenticate: Basic realm="Selfphp');
+  print 'この画面へのアクセスは認められませんでした。';
+  die();
+}else {
+  if($_SERVER['PHP_AUTH_USER'] === 'admin_user' &&
+  $_SERVER['PHP_AUTH_PW'] === 'admin_pass'){
+  print '正しく認証が行われました。';
+} else {
+  print 'ユーザー名、またはパスワードが間違っています。';
+}
+}
---
+++ practice/section8/cache.php
@@ -0,0 +1,9 @@
+<?php
+// header('Expires: Sun, 15 Jan 1970 00:00:00 GMT');
+// header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+// header('Cache-Control: no-store, no-cache, must-revalidate');
+
+// 自分で書いたコード
+header('Exprise: Sun,15 Jan 1970 00:00:00 GMT');
+header('Last-modified: ' .gmdate('D, d M Y H:i:s') . 'GMT');
+header('Cache-control: no-store,no-cashe, must-reva;idate');
