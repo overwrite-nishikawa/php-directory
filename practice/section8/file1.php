90694e94c9eebbf12dc79c8a80e75ae33cadcb95
Kisara Nishikawa <nishikawa@overwrite.work>
2023-01-09 23:05:07 +0900

    section8のfile1.php~section9のfile1.phpまで

---
+++ practice/section8/file1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8" />
+<title>アップロード処理</title>
+</head>
+<body>
+<!-- <form method="POST" action="file2.php" enctype="multipart/form-data">
+<label for="upfile">ファイルのパス：</label>
+<input type="hidden" name="max_file_size" value="1000000" />
+<input id="upfile" type="file" name="upfile" size="40" />
+<input type="submit" value="アップロード" /> -->
+<!-- </form> -->
+<form method="post" action="file2.php" enctype="multipart/form-data">
+    <label for="upfile">ファイルのパス:</label>
+    <input type="hidden" name="max_file_size" value="1000000">
+    <input type="file" id="upfile" name="upfile" size="40">
+    <input type="submit" value="アップロード">
+</form>
+</body>
+</html>
