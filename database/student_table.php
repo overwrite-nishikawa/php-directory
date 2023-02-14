0147252b6f589be8cce64678c7dc3e099c258af7
Kisara Nishikawa <nishikawa@overwrite.work>
2023-02-14 22:32:33 +0900

    データベースのファイル名を変更

---
+++ database/student_table.php
@@ -0,0 +1,25 @@
+<?=
+require_once '../DbManager.php';
+
+$db = getDb();
+
+$fp = fopen('../csv/student_table.csv','r');
+
+while($line = fgetcsv($fp)) {
+  var_dump($line);
+  $data = $db->prepare("INSERT INTO student_table(
+    student_number,
+    name,
+    jender
+  )
+  VALUES
+  (
+    $line[0],
+    '".$line[1]."',
+    '".$line[2]."'
+  )");
+
+  $data->execute();
+};
+
+fclose($fp);
---
+++ database/student_table_number.php
@@ -0,0 +1,21 @@
+<?=
+require_once '../DbManager.php';
+
+$db = getDb();
+
+$fp = fopen('../csv/student_table_number.csv','r');
+while($line = fgetcsv($fp)) {
+    var_dump($line);
+    $data2 = $db->prepare("INSERT INTO student_table_number(
+        student_number,
+        subject,
+        point
+        )
+        VALUES(
+            $line[0],'".$line[1]."',$line[2]
+        )");
+
+$data2->execute();
+}
+
+fclose($fp);
